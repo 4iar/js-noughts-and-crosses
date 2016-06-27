542cdbd479c4bbb07a5d7d4f28f87bc6ead183b8
4iar <4iar@users.noreply.github.com>
2016-06-27 14:58:28 +0100

    Add basic 'ai'

--- src/js/computeNextMove.js
+++ src/js/computeNextMove.js
@@ -1,3 +1,29 @@
-export default function computeNextMove(grid) {
-  return {col: 1, row: 0};
-}
+import _ from 'lodash';
+
+import checkIfWinner from './checkIfWinner';
+
+
+const findEmptyPositions = (grid) => {
+  var emptyPositions = [];
+  
+  
+  for (var rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+    const row = grid[rowIndex];
+
+    for (var colIndex = 0; colIndex < row.length; colIndex++) {
+      const square = row[colIndex];
+      if (!square) {
+        emptyPositions.push({row: rowIndex, col: colIndex});
+      };
+    };
+  };
+  return emptyPositions;
+};
+
+
+const computeNextMove = (grid) => {
+  return _.sample(findEmptyPositions(grid));
+};
+
+
+export default computeNextMove;
